7f72b028545b9f33d080cd776ffe5b4b1dd9e351
JackKCWong <jiajue.huang@live.com>
2025-03-03 22:40:29 +0800

    add grep

--- pdutils.py
+++ pdutils.py
@@ -14,16 +14,32 @@ class HttpAccessor:
             raise AttributeError("a pd.Series is required")
 
     def get(self, concurrency=50):
-        return asyncio.run(self.__get())
+        resps = asyncio.run(self.__get())
+        return pd.Series(resps, index=self._obj.index)
 
     async def __get(self):
         urls = self._obj.tolist()
         async def do_get(session, url):
-            print(f"GET {url}")
-            async with session.get(url) as resp:
-                return [resp.status, await resp.text()]
+            try:
+                async with session.get(url) as resp:
+                    return [resp.status, await resp.text()]
+            except Exception as e:
+                return [0, str(e)]
 
         async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
             tasks = [asyncio.create_task(do_get(session, url)) for url in urls]
             return await asyncio.gather(*tasks)
         
+
+@pd.api.extensions.register_dataframe_accessor("txt")
+class TextAccessor:
+    def __init__(self, pandas_obj):
+        if not isinstance(pandas_obj, pd.DataFrame):
+            raise AttributeError("a pd.DataFrame is required")
+        self._df = pandas_obj
+
+    def grep(self, text):
+        return self._df[self._df.apply(lambda row: row.astype(str).str.contains(text, case=False, regex=False).any(), axis=1)]
+
+    def egrep(self, pat):
+        return self._df[self._df.apply(lambda row: row.astype(str).str.contains(pat).any(), axis=1)]
--- test_pdutils.py
+++ test_pdutils.py
@@ -3,16 +3,28 @@ import pdutils
 import pytest
 
 
-# @pytest.mark.asyncio
-def test_get():
+def test_http_get():
     urls = pd.Series([
         # f"http://localhost:8080/200",
         # f"http://localhost:8080/400"
         "https://httpbin.org/status/200",
         "https://httpbin.org/status/400",
-    ] * 20) 
+    ]) 
 
     resps = urls.http.get()
     assert len(resps) == len(urls)
     assert resps[0][0] == 200
     assert resps[1][0] == 400
+
+
+def test_df_grep():
+    df = pd.DataFrame({
+        'A': [1, 2, 3],
+        'B': ["a", "b", "C"]
+    })
+
+    assert df.txt.grep("1")["A"].iloc[0] == 1
+    assert df.txt.grep("b")["A"].iloc[0] == 2
+    assert df.txt.grep("c")["A"].iloc[0] == 3
+
+    assert len(df.txt.egrep("a|b")) == 2
